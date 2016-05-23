b4f3517ddcfe88984e299264c298d5b639433973
Koike Takaaki <taka.vao.321@gmail.com>
2016-05-23 15:43:35 +0900

    後ろ向きdroid君に変更
    javaによるimage表示

--- app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ryokun.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("DroidSlot");
+        ImageView droidImage1 = (ImageView)this.findViewById(R.id.droidimageid1);
+        droidImage1.setImageResource(R.drawable.droid_back);
     }
 }
