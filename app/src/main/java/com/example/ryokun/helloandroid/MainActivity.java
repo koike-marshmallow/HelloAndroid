b771b1ab0861d1cfdc8b48603a0040b57ae1cc36
Koike Takaaki <taka.vao.321@gmail.com>
2016-05-23 15:49:29 +0900

    起動時にランダムで画像が変わるように変更

--- app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("DroidSlot");
+
         ImageView droidImage1 = (ImageView)this.findViewById(R.id.droidimageid1);
-        droidImage1.setImageResource(R.drawable.droid_back);
+        int droidSide1;
+        Random r = new Random();
+
+        droidSide1 = r.nextInt(2);
+        if( droidSide1 == 0 ){
+            droidImage1.setImageResource(R.drawable.droid_front);
+        }else{
+            droidImage1.setImageResource(R.drawable.droid_back);
+        }
     }
 }
