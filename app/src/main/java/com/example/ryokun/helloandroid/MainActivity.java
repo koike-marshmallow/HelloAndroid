f4b77107f6a9950412a5e0036c7b8b65b49b4848
Koike Takaaki <taka.vao.321@gmail.com>
2016-05-23 16:23:26 +0900

    スタートボタンを追加

--- app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/ryokun/helloandroid/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.ryokun.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Button;
 
 import java.util.Random;
 
@@ -14,23 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.setTitle("DroidSlot");
 
-        ImageView droidImage1 = (ImageView)this.findViewById(R.id.droidimageid1);
-        int droidSide1;
-        Random r = new Random();
+        final ImageView droidImage1 = (ImageView)this.findViewById(R.id.droidimageid1);
+        final Random r = new Random();
+        Button b1 = (Button) this.findViewById(R.id.slotbutton1);
+        b1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int droidSide1, drawableId;
 
-        droidSide1 = r.nextInt(4);
-        switch( droidSide1 ) {
-            case 0:
-                droidImage1.setImageResource(R.drawable.droid_front);
-                break;
-            case 1:
-                droidImage1.setImageResource(R.drawable.droid_back);
-                break;
-            case 2:
-                droidImage1.setImageResource(R.drawable.droid_left);
-                break;
-            default:
-                droidImage1.setImageResource(R.drawable.droid_right);
-        }
+                droidSide1 = r.nextInt(4);
+                switch( droidSide1 ){
+                    case 0:
+                        drawableId = R.drawable.droid_front; break;
+                    case 1:
+                        drawableId = R.drawable.droid_back; break;
+                    case 2:
+                        drawableId = R.drawable.droid_left; break;
+                    default:
+                        drawableId = R.drawable.droid_right;
+                }
+                droidImage1.setImageResource(drawableId);
+            }
+        });
     }
 }
